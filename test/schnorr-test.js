02285fa538e6fb61db8c49d7d52c587d0d451c14
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-09-28 18:00:46 -0700

    schnorr: update bcrypto.

--- test/schnorr-test.js
+++ test/schnorr-test.js
@@ -4,12 +4,13 @@
 'use strict';
 
 const assert = require('./util/assert');
-const {secp256k1, hash256} = require('bcrypto');
+const secp256k1 = require('bcrypto/lib/secp256k1');
+const hash256 = require('bcrypto/lib/hash256');
 const schnorr = require('../');
 
 describe('Schnorr', function() {
   it('should do proper schnorr', () => {
-    const key = secp256k1.generatePrivateKey();
+    const key = secp256k1.privateKeyGenerate();
     const pub = secp256k1.publicKeyCreate(key, true);
     const msg = hash256.digest(Buffer.from('foo', 'ascii'));
     const sig = schnorr.sign(msg, key);
